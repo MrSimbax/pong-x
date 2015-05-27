026f72c115992b5794c19a7d15a785fe65ba2d1f
MrSimbax <simbaxlp@gmail.com>
2015-05-27 13:37:37 +0200

    Add player controllers

---
+++ Pong-X/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public string axis = "Vertical";
+
+    private Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector2(0, Input.GetAxis(axis) * speed);
+    }
+}
