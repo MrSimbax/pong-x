018b5fd1be1a54c744e1ffa5741d8c140fbab654
MrSimbax <simbaxlp@gmail.com>
2015-05-28 12:59:30 +0200

    Add script showing current scores

---
+++ Pong-X/Assets/Scripts/UIScore.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(Text))]
+public class UIScore : MonoBehaviour {
+
+    public PlayerController player;
+
+    private Text text;
+	
+    void Start()
+    {
+        text = GetComponent<Text>();
+        if (player == null)
+        {
+            throw new MissingReferenceException();
+        }
+    }
+
+    void Update()
+    {
+        text.text = "" + player.score;
+    }
+}
