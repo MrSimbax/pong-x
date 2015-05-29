7e04057e6b1d4ad1f9ee9238de36977558edcfba
MrSimbax <simbaxlp@gmail.com>
2015-05-29 19:38:47 +0200

    Remove OnCollisionExit2D

--- Pong-X/Assets/Scripts/Controllers/BallController.cs
+++ Pong-X/Assets/Scripts/Controllers/BallController.cs
@@ -47,11 +47,6 @@ public class BallController : MonoBehaviour
         rigidbody.velocity = new Vector2(x, y) * speed;
     }
 
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        
-    }
-
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
