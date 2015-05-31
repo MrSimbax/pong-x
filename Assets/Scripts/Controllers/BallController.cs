d4e13252637785db83a3a7f27682e372246f477d
MrSimbax <simbaxlp@gmail.com>
2015-05-31 13:39:43 +0200

    Separate hit sounds to class

--- Assets/Scripts/Controllers/BallController.cs
+++ Assets/Scripts/Controllers/BallController.cs
@@ -2,28 +2,23 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
-[RequireComponent(typeof(AudioSource))]
 public class BallController : MonoBehaviour
 {
     [Range(0.0f, 1000.0f)] public float speed;
     [Range(0.0f, 1000.0f)] public float maxSpeed;
-    public AudioClip soundHitPlayer;
-    public AudioClip soundHitWall;
 
     private new Rigidbody2D rigidbody;
     private new BoxCollider2D collider;
-    private AudioSource audioSource;
     private Vector2 initialPosition;
     private Vector2 previousVelocity;
 
     public delegate void ReachEndAction();
     public static event ReachEndAction OnReachedEnd;
 
-	void Start ()
+	void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
         collider = GetComponent<BoxCollider2D>();
-        audioSource = GetComponent<AudioSource>();
         initialPosition = transform.position;
         Mathf.Clamp(speed, 0.0f, maxSpeed);
 	}
@@ -59,9 +54,6 @@ public class BallController : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            audioSource.clip = soundHitPlayer;
-            audioSource.Play();
-            
             // Assume the ball and the pad have the same mass
             // A momentum conservation law holds
             // 
@@ -83,12 +75,6 @@ public class BallController : MonoBehaviour
             y = Mathf.Sign(y) * Mathf.Clamp(Mathf.Abs(y), 0.0f, maxSpeed);
 
             rigidbody.velocity = new Vector2(x, y);
-        }
-        
-        if (collision.gameObject.tag == "WallNormal")
-        {
-            audioSource.clip = soundHitWall;
-            audioSource.Play();
         }
 
         if (collision.gameObject.tag == "WallEnd" && OnReachedEnd != null)
---
+++ Assets/Scripts/Sound/HitSoundPlayer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(AudioSource))]
+public class HitSoundPlayer : MonoBehaviour
+{
+    [System.Serializable]
+    public struct HitSound
+    {
+        public AudioClip clip;
+        public string tag;
+    }
+    
+    public HitSound[] hitSounds;
+    
+    private AudioSource audioSource;
+    private Dictionary<string, AudioClip> sounds;
+    
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        sounds = new Dictionary<string, AudioClip>();
+        foreach(HitSound sound in hitSounds)
+        {
+            sounds[sound.tag] = sound.clip;
+        }
+    }
+    
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (sounds.ContainsKey(col.gameObject.tag))
+        {
+            audioSource.clip = sounds[col.gameObject.tag];
+            audioSource.Play();
+        }
+    }
+	
+}
